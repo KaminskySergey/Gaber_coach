8cf49e9073b1eedf61fc51cd80728ec8bd0df8af
Serhii Kaminskyi <kaminsky_s_s@ukr.net>
2023-11-23 17:11:56 +0200

    redux

---
+++ src/app/redux/store.ts
@@ -0,0 +1,37 @@
+import { AnyAction, Reducer, configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistPartial } from "redux-persist/es/persistReducer";
+
+const authPersistConfig = {
+  key: "auth",
+  storage,
+};
+
+export const store = configureStore({
+  reducer: {
+        // users: usersReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
